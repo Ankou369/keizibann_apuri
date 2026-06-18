e0346ab94866e57e3a5213302636614a7e2c7ced
kouanya1009 <kouanya1009@outlook.jp>
2026-06-18 15:57:21 +0900

    ハンバーガーメニューの追加の途中
    色の変更、profileとaboutのhtmlでボタンの追加が必要

--- app.py
+++ app.py
@@ -168,6 +168,14 @@ def uploaded_file(filename):
         filename
     )
 
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
 
